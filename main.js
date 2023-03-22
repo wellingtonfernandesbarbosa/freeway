6223cb5dbfb383311f71a88e0c6d9aefe1de8488
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-03-21 21:34:03 -0300

    Elementos iniciais do jogo

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
+let imagemDaEstrada;
+
+// Ator
+let imagemAtor;
+let xAtor = 100;
+let yAtor = 366;
+
+// Carro
+let imagemCarro;
+let xCarro = 500;
+let yCarro = 40;
+
+function preload() {
+    imagemDaEstrada = loadImage("img/estrada.png")
+    imagemAtor = loadImage("img/ator-1.png");
+    imagemCarro = loadImage("img/carro-1.png")
+};
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 400);
 };
 
 function draw() {
-    background(220);
+    background(imagemDaEstrada);
+    mostraAtor();
+    mostraCarro();
+    movimentaCarro();
+    movimentaAtor();
 };
+
+function mostraAtor(){
+    image(imagemAtor, xAtor, yAtor, 30, 30);
+};
+
+function mostraCarro(){
+    image(imagemCarro, xCarro, yCarro, 50, 40);
+};
+
+function movimentaCarro(){
+    xCarro -= 2;
+};
+
+function movimentaAtor(){
+    if (keyIsDown(UP_ARROW)){
+        yAtor -= 3;
+    } else if (keyIsDown(DOWN_ARROW)){
+        yAtor += 3;
+    }
+}
